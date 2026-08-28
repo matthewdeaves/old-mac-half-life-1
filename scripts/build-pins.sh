4aa45ea68da0a63f1eb40ed83d66bd76e9385074
Matthew Deaves <mndeaves@me.com>
2026-08-28 09:13:56 +0100

    Build: pin engine commit 1bad7f77, fix typed letters firing menu hotkeys
    Refs #18.

--- scripts/build-pins.sh
+++ scripts/build-pins.sh
@@ -35,7 +35,7 @@ XASH_VERSION="0.21"
 # commit here is the one the fleet A/Bs and the G3 hands-on pass ran against.
 PIN_ENGINE_URL="https://github.com/matthewdeaves/xash3d-fwgs.git"
 PIN_ENGINE_BRANCH="oldmac"
-PIN_ENGINE_COMMIT="b1704dfca11c98f75694bf4557a5abc47b2e28a3"
+PIN_ENGINE_COMMIT="1bad7f77345de54b4f40e526f4f5e09ff4d08f6a"
 PIN_ENGINE_UPSTREAM="FWGS/xash3d-fwgs@f0ea3a19"
 
 # --- menu: submodule of the engine, re-pointed by our engine branch ----------
